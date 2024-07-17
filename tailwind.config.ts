b97b5ecb2579b1de3fe61c677e6d224a05c2bfe3
Facug03 <facundogonzales1550@gmail.com>
2024-07-16 23:12:17 -0300

    add colors and font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,42 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: 'rgba(233, 225, 38, 1)',
+        b: 'rgba(30, 30, 29, 1)',
+        'b-75': 'rgba(30, 30, 29, 0.75)',
+        'b-50': 'rgba(30, 30, 29, 0.5)',
+        'b-25': 'rgba(30, 30, 29, 0.25)',
+        'b-15': 'rgba(30, 30, 29, 0.15)',
+        'b-10': 'rgba(30, 30, 29, 0.1)',
+        'b-5': 'rgba(30, 30, 29, 0.05)',
+        w: 'rgba(243, 243, 237, 1)',
+        'w-75': 'rgba(243, 243, 237, 0.75)',
+        'w-50': 'rgba(243, 243, 237, 0.5)',
+        'w-25': 'rgba(243, 243, 237, 0.25)',
+        'w-15': 'rgba(243, 243, 237, 0.15)',
+        'w-10': 'rgba(243, 243, 237, 0.1)',
+        'w-5': 'rgba(243, 243, 237, 0.05)',
+      },
+      fontSize: {
+        t1: '42px',
+        t2: '35px',
+        t3: '29px',
+        t4: '24px',
+        t5: '20px',
+        t6: '16px',
+        t7: '14px',
+        t8: '12px',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
