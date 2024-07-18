14f826b997089ee00a10ab8ea5f04ed104e00c61
Facug03 <facundogonzales1550@gmail.com>
2024-07-17 22:01:57 -0300

    add hero and slider to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import Hero from '@/sections/Hero'
+import Slider from '@/sections/slider/Slider'
+
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <main className='max-w-[1380px] w-full'>
+      <Hero />
+      <Slider title='In theathers' />
+      <Slider title='Top rated' />
+      <Slider title='Popular' />
+    </main>
   )
 }
