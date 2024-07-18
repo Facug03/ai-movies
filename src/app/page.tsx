bb50b827367cfe951b4bbce59c2372d7ed321803
Facug03 <facundogonzales1550@gmail.com>
2024-07-18 20:32:12 -0300

    remove main element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Slider from '@/sections/slider/Slider'
 
 export default function Home() {
   return (
-    <main className='max-w-[1380px] w-full'>
+    <>
       <Hero />
       <Slider title='In theathers' />
       <Slider title='Top rated' />
       <Slider title='Popular' />
-    </main>
+    </>
   )
 }
