7085add7a5205a3adc585bd4869509e2125dae97
Facug03 <facundogonzales1550@gmail.com>
2024-07-18 20:31:27 -0300

    add fade-in animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -36,6 +36,15 @@ const config: Config = {
         t7: '14px',
         t8: '12px',
       },
+      animation: {
+        'fade-in': 'fade-in 0.3s ease-in both',
+      },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
     },
   },
   plugins: [],
