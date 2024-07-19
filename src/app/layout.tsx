754121ef59e51a842c69b974b55e628ad6cb293f
Facug03 <facundogonzales1550@gmail.com>
2024-07-19 20:31:37 -0300

    format code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,11 @@ const schibstedGrotesk = Schibsted_Grotesk({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'aiMovies',
-  description: 'Web to see movies',
+  description: 'Web to see movies'
 }
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode
 }>) {
--- src/consts/imageSizes.ts
+++ src/consts/imageSizes.ts
@@ -1,4 +1,4 @@
 export const imageSizes = {
   '1920x800': 'w1920_and_h800_multi_faces',
-  '220x330': 'w220_and_h330_face',
+  '220x330': 'w220_and_h330_face'
 } as const
