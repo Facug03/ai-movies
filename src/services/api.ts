ae4e7c5f2c23ca0ec9af3356d999c12344b83047
Facug03 <facundogonzales1550@gmail.com>
2024-07-22 15:41:01 -0300

    add and clean apis urls

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,20 +2,22 @@ export const baseUrl = 'https://api.themoviedb.org/3'
 
 export const apis = {
   movies: {
-    nowPlaying: `${baseUrl}/movie/now_playing?language=en-US&page=1`,
-    popular: `${baseUrl}/movie/popular?language=en-US&page=1`,
-    topRated: `${baseUrl}/movie/top_rated?language=en-US&page=1`,
-    upcoming: `${baseUrl}/movie/upcoming?language=en-US&page=1`
+    nowPlaying: `${baseUrl}/movie/now_playing?`,
+    popular: `${baseUrl}/discover/movie?sort_by=popularity.desc`,
+    topRated: `${baseUrl}/movie/top_rated?`,
+    upcoming: `${baseUrl}/movie/upcoming?`,
+    genres: `${baseUrl}/genre/movie/list?language=en`
   },
   series: {
-    onTheAir: `${baseUrl}/tv/on_the_air?language=en-US&page=1`,
-    popular: `${baseUrl}/tv/popular?language=en-US&page=1`,
-    topRated: `${baseUrl}/tv/top_rated?language=en-US&page=1`,
-    upcoming: `${baseUrl}/tv/upcoming?language=en-US&page=1`,
-    anime: `${baseUrl}/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=vote_average.desc&vote_count.gte=300&with_genres=16&with_origin_country=JP`
+    onTheAir: `${baseUrl}/tv/on_the_air?`,
+    popular: `${baseUrl}/discover/tv?sort_by=popularity.desc`,
+    topRated: `${baseUrl}/tv/top_rated?`,
+    upcoming: `${baseUrl}/tv/upcoming?`,
+    anime: `${baseUrl}/discover/tv?include_adult=false&include_null_first_air_dates=false&sort_by=vote_average.desc&vote_count.gte=300&with_genres=16&with_origin_country=JP`,
+    genres: `${baseUrl}/genre/tv/list?language=en`
   },
   all: {
-    trending: `${baseUrl}/trending/all/week?language=en-US&page=1`,
-    search: `${baseUrl}/search/multi?language=en-US&include_adult=false`
+    trending: `${baseUrl}/trending/all/week`,
+    search: `${baseUrl}/search/multi?`
   }
 }
