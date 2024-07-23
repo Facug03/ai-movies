960b63bea8449fb9188f6374cda5d7532c8d0ae0
Facug03 <facundogonzales1550@gmail.com>
2024-07-23 20:47:23 -0300

    improve button responsive

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,14 +7,14 @@ interface Props {
 export default function Button({ text, type, icon }: Props) {
   if (type === 'secondary') {
     return (
-      <button className='flex w-fit items-center justify-between gap-3 rounded-lg bg-w px-8 py-2 font-bold text-b'>
+      <button className='flex w-fit items-center justify-between gap-3 rounded-lg bg-w px-8 py-2 text-m-t6 font-bold text-b sm:text-t6'>
         {text} {icon}
       </button>
     )
   }
 
   return (
-    <button className='flex w-fit items-center justify-between gap-3 rounded-lg bg-primary px-8 py-2 font-bold text-b'>
+    <button className='flex w-fit items-center justify-between gap-3 rounded-lg bg-primary px-8 py-2 text-m-t6 font-bold text-b sm:text-t6'>
       {text} {icon}
     </button>
   )
