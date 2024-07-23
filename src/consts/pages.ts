5cb730c8a7662678814d3d2c971d38b9d2e08331
Facug03 <facundogonzales1550@gmail.com>
2024-07-23 20:47:36 -0300

    add pages urls

---
+++ src/consts/pages.ts
@@ -0,0 +1,22 @@
+export const pages = [
+  {
+    name: 'Home',
+    href: '/'
+  },
+  {
+    name: 'Search',
+    href: '/search'
+  },
+  {
+    name: 'Movies',
+    href: '/movies'
+  },
+  {
+    name: 'Series',
+    href: '/series'
+  },
+  {
+    name: 'Animes',
+    href: '/animes/series'
+  }
+]
