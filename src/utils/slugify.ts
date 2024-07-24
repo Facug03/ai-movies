36b793aada68e652531679bbf9d0a1df7eb6bca2
Facug03 <facundogonzales1550@gmail.com>
2024-07-23 22:42:13 -0300

    add suport for speacial characters

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,6 +1,11 @@
 export const slugify = (str: string) => {
   str = str.replace(/^\s+|\s+$/g, '')
   str = str.toLowerCase()
+  const from = 'áàäâãéèëêíìïîóòöôõúùüûñç'
+  const to = 'aaaaaeeeeiiiiooooouuuunc'
+  for (let i = 0; i < from.length; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i))
+  }
   str = str
     .replace(/[^a-z0-9 -]/g, '')
     .replace(/\s+/g, '-')
