a2e1a28f89d7d2f7d350e4b79f59866bf4e0eb8e
Facug03 <facundogonzales1550@gmail.com>
2024-07-23 22:43:07 -0300

    change name of files

---
+++ src/app/animes/series/[slug]/page.tsx
@@ -0,0 +1,41 @@
+import MediaPage from '@/components/MediaPage'
+import { getSeries, getSeriesGenres } from '@/services/series'
+import { slugify } from '@/utils/slugify'
+
+export default async function AnimesSeriesGenres({ params: { slug } }: { params: { slug: string } }) {
+  const splitSlug = slug.split('-')
+  const [errorMovie, dataMovie] = await getSeries('anime', {
+    page: 1,
+    genres: [16, Number(splitSlug[splitSlug.length - 1])],
+    language: 'en-US'
+  })
+  const [errorGenre, dataGenre] = await getSeriesGenres()
+
+  if (errorGenre || errorMovie) {
+    return <div>Error</div>
+  }
+
+  return (
+    <MediaPage
+      genres={dataGenre.genres}
+      mediaContent={dataMovie}
+      title='Animes'
+      page='animes/series'
+      dropdownTitle={dataGenre.genres.find((genre) => genre.id === Number(splitSlug[splitSlug.length - 1]))?.name}
+    />
+  )
+}
+
+export const dynamicParams = false
+
+export async function generateStaticParams() {
+  const [error, data] = await getSeriesGenres()
+
+  if (error) {
+    throw error
+  }
+
+  return data.genres.map((genre) => ({
+    slug: `${slugify(genre.name)}-${genre.id}`
+  }))
+}
--- src/services/movies.ts
+++ src/services/movies.ts
@@ -84,3 +84,53 @@ export const getMoviesGenres = async (): Promise<[null, Genres] | [Error, null]>
     return [new Error('Something went wrong'), null]
   }
 }
+
+export const getMovie = async (id: number): Promise<[null, Media] | [Error, null]> => {
+  try {
+    const defaultFilters = {
+      page: 1,
+      language: 'en-US',
+      genres: []
+    }
+    const url = applyFilters(apis.movies[type], filters ?? defaultFilters)
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMzA5MmUxNzhjZmIzMjRhYTY1OGI3NGZmZDExMWY1MiIsIm5iZiI6MTcyMTE3NzY4MS4wMjEwNjIsInN1YiI6IjY2OTE1MTAzNjM2YzQyN2EwNzllMjZjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rUzH7x6w74nmTOT5ioSc2_ZQ1795NK1BFsim7UeovVw'
+      }
+    })
+
+    if (!response.ok) {
+      throw new Error(`Unknown error: ${response.status}`)
+    }
+
+    const data = (await response.json()) as Data
+    const formatData: Media[] = data.results.map((media) => {
+      return {
+        backdropPath: media.backdrop_path,
+        id: media.id,
+        originalLanguage: media.original_language,
+        originalTitle: media.original_title,
+        overview: media.overview,
+        popularity: media.popularity,
+        posterPath: media.poster_path,
+        releaseDate: media.release_date,
+        title: media.title,
+        voteAverage: media.vote_average,
+        voteCount: media.vote_count,
+        type: 'Movie'
+      }
+    })
+
+    return [null, formatData]
+  } catch (error) {
+    if (error instanceof Error) {
+      return [error, null]
+    }
+
+    return [new Error('Something went wrong'), null]
+  }
+}
