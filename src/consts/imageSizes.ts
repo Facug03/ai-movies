4f1c02d9c020a622744d45beeac5e762d149cb9c
Facug03 <facundogonzales1550@gmail.com>
2024-07-25 18:30:30 -0300

    add new size

--- src/consts/imageSizes.ts
+++ src/consts/imageSizes.ts
@@ -1,4 +1,7 @@
 export const imageSizes = {
   '1920x800': 'w1920_and_h800_multi_faces',
+  1280: 'w1280',
+  '533x300': 'w533_and_h300_bestv2',
+  '300x450': 'w300_and_h450_bestv2',
   '220x330': 'w220_and_h330_face'
 } as const
