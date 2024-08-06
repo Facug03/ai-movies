bfc7f3a65be0159752e3bb5bd59de6ffd3e46dc0
Facug03 <facundogonzales1550@gmail.com>
2024-08-05 22:07:44 -0300

    add more options

--- src/components/Chat/TabsSlider.tsx
+++ src/components/Chat/TabsSlider.tsx
@@ -44,8 +44,8 @@ function MoviesTabs({ title, reload, setMessages }: Props) {
   return (
     <>
       <Tab onClick={() => onClickSubmit(`Find me something similar to ${title}`)}>Similar</Tab>
-      <Tab onClick={() => onClickSubmit('What are the reviews?')}>Reviews</Tab>
-      <Tab onClick={() => onClickSubmit('Could you give me a summary?')}>Summary</Tab>
+      <Tab onClick={() => onClickSubmit(`What are the reviews of ${title}?`)}>Reviews</Tab>
+      <Tab onClick={() => onClickSubmit(`Could you give me a summary of ${title}?`)}>Summary</Tab>
       <Tab onClick={() => onClickSubmit(`Are there upcoming movies of ${title}`)}>Upcoming movies</Tab>
       <Tab onClick={() => onClickSubmit(`Let's play a trivia about ${title}`)}>Trivia</Tab>
       <Tab onClick={() => onClickSubmit(`When ${title} was released?`)}>Release</Tab>
@@ -55,6 +55,12 @@ function MoviesTabs({ title, reload, setMessages }: Props) {
       <Tab onClick={() => onClickSubmit(`Was ${title} based on a book, comic book, or other source material??`)}>
         Adaptations
       </Tab>
+      <Tab onClick={() => onClickSubmit(`Who directed ${title}?`)}>Director</Tab>
+      <Tab onClick={() => onClickSubmit(`Did ${title} win any awards?`)}>Awards</Tab>
+      <Tab onClick={() => onClickSubmit(`What are the genres of ${title}?`)}>Genres</Tab>
+      <Tab onClick={() => onClickSubmit(`What songs are in the soundtrack of ${title}?`)}>Soundtrack</Tab>
+      <Tab onClick={() => onClickSubmit(`What are some popular fan theories about ${title}?`)}>Fan Theories</Tab>
+      <Tab onClick={() => onClickSubmit(`Are there any easter eggs in ${title}?`)}>Easter Eggs</Tab>
     </>
   )
 }
@@ -68,18 +74,26 @@ function SeriesTabs({ title, reload, setMessages }: Props) {
   return (
     <>
       <Tab onClick={() => onClickSubmit(`Find me something similar to ${title}`)}>Similar</Tab>
-      <Tab onClick={() => onClickSubmit('What are the reviews?')}>Reviews</Tab>
-      <Tab onClick={() => onClickSubmit('Could you give me a summary?')}>Summary</Tab>
+      <Tab onClick={() => onClickSubmit(`What are the reviews of ${title}?`)}>Reviews</Tab>
+      <Tab onClick={() => onClickSubmit(`Could you give me a summary of ${title}?`)}>Summary</Tab>
+      <Tab onClick={() => onClickSubmit(`Which network airs ${title}?`)}>Network</Tab>
       <Tab onClick={() => onClickSubmit(`Are there any upcoming season of ${title}?`)}>Upcoming seasons</Tab>
+      <Tab onClick={() => onClickSubmit(`Are there any upcoming episodes of ${title}?`)}>Upcoming episodes</Tab>
       <Tab onClick={() => onClickSubmit(`Let's play a trivia about ${title}`)}>Trivia</Tab>
       <Tab onClick={() => onClickSubmit(`When ${title} was released?`)}>Release</Tab>
-      <Tab onClick={() => onClickSubmit('How many seasons and episodes does it have?')}>Seasons and episodes</Tab>
+      <Tab onClick={() => onClickSubmit(`How many seasons and episodes does ${title} have?`)}>Seasons and episodes</Tab>
       <Tab onClick={() => onClickSubmit(`How was ${title} made?`)}>Production</Tab>
       <Tab onClick={() => onClickSubmit(`Who are the main characters of ${title}?`)}>Characters</Tab>
       <Tab onClick={() => onClickSubmit(`What is the plot of ${title}?`)}>Plot</Tab>
       <Tab onClick={() => onClickSubmit(`Was ${title} based on a book, comic book, or other source material??`)}>
         Adaptations
       </Tab>
+      <Tab onClick={() => onClickSubmit(`Who directed ${title}?`)}>Director</Tab>
+      <Tab onClick={() => onClickSubmit(`Did ${title} win any awards?`)}>Awards</Tab>
+      <Tab onClick={() => onClickSubmit(`What are the genres of ${title}?`)}>Genres</Tab>
+      <Tab onClick={() => onClickSubmit(`What songs are in the soundtrack of ${title}?`)}>Soundtrack</Tab>
+      <Tab onClick={() => onClickSubmit(`What are some popular fan theories about ${title}?`)}>Fan Theories</Tab>
+      <Tab onClick={() => onClickSubmit(`Are there any easter eggs in ${title}?`)}>Easter Eggs</Tab>
     </>
   )
 }
@@ -96,7 +110,7 @@ function GeneralTabs({ reload, setMessages, setInput }: Props) {
 
   return (
     <>
-      <Tab onClick={() => onClickSubmit('Find me something similar')}>Recommendations</Tab>
+      <Tab onClick={() => onClickSubmit('What do you recommend me based on my favorites?')}>Recommendations</Tab>
       <Tab onClick={() => onClickAddMessage('What are the reviews of ')}>Reviews</Tab>
       <Tab onClick={() => onClickAddMessage('Could you give me a summary of ')}>Summary</Tab>
       <Tab onClick={() => onClickAddMessage('Find me something similar to ')}>Find me something similar to...</Tab>
