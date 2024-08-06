61457e2e2c5879d589007f87a7515a3cd3f4691e
Facug03 <facundogonzales1550@gmail.com>
2024-08-05 22:05:24 -0300

    hook to reutilize favorites logic

---
+++ src/hooks/use-favorites.ts
@@ -0,0 +1,58 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import { Media } from '@/types/media'
+
+export function useFavorites() {
+  const [favorites, setFavorites] = useState<Media[] | null>(null)
+  const isLoading = favorites === null
+
+  useEffect(() => {
+    const favoritesLs = localStorage.getItem('favorites')
+
+    if (!favoritesLs) return
+
+    const favoritesParsed = favoritesLs ? (JSON.parse(favoritesLs) as Media[]) : []
+    setFavorites(favoritesParsed)
+  }, [])
+
+  const addFavorite = (media: Media) => {
+    const favoritesLs = localStorage.getItem('favorites')
+
+    if (favoritesLs) {
+      const parsedFavorites = JSON.parse(favoritesLs)
+      localStorage.setItem('favorites', JSON.stringify([...parsedFavorites, media]))
+      setFavorites((prev) => [...(prev ?? []), media])
+
+      return
+    }
+
+    localStorage.setItem('favorites', JSON.stringify([media]))
+    setFavorites((prev) => [...(prev ?? []), media])
+  }
+
+  const removeFavorite = (media: Media) => {
+    const favoritesLs = localStorage.getItem('favorites')
+
+    if (!favoritesLs) return
+
+    const parsedFavorites = JSON.parse(favoritesLs)
+    localStorage.setItem(
+      'favorites',
+      JSON.stringify(parsedFavorites.filter((favorite: Media) => favorite.id !== media.id))
+    )
+    setFavorites((prev) => {
+      if (!prev) return null
+
+      return [...prev.filter((favorite) => favorite.id !== media.id)]
+    })
+  }
+
+  return {
+    favorites,
+    addFavorite,
+    removeFavorite,
+    isLoading
+  }
+}
