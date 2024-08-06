287557b78cd177377ec549a7863234db853da079
Facug03 <facundogonzales1550@gmail.com>
2024-08-06 01:27:10 -0300

    change file names to snake_case

---
+++ src/components/chat/all-chats.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useChatStore } from '@/providers/chat-store-provider'
+import Chat from './chat'
+
+export default function AllChats() {
+  const chats = useChatStore((state) => state.chats)
+
+  return (
+    <>
+      {chats.map((chat) => {
+        if (!chat.isOpen) return null
+
+        return <Chat key={chat.id} id={chat.id} />
+      })}
+    </>
+  )
+}
