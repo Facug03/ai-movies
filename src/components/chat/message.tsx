796ab00270c27616c9665a9dad3b6ae095246910
Facug03 <facundogonzales1550@gmail.com>
2024-08-06 02:21:55 -0300

    add function to compare props

--- src/components/chat/message.tsx
+++ src/components/chat/message.tsx
@@ -10,40 +10,44 @@ interface Props {
   message: MessageAi
 }
 
-export const Message = memo(function Message({ message }: Props) {
-  if (message.role !== 'user' && message.role !== 'assistant') return
+export const Message = memo(
+  function Message({ message }: Props) {
+    console.log({ message })
+    if (message.role !== 'user' && message.role !== 'assistant') return null
 
-  if (message.role === 'user') {
+    if (message.role === 'user') {
+      return (
+        <div className='flex justify-end'>
+          <p className='rounded-xl bg-w-10 px-3 py-1 text-tinherit text-w'>{message.content}</p>
+        </div>
+      )
+    }
+
     return (
-      <div className='flex justify-end'>
-        <p className='rounded-xl bg-w-10 px-3 py-1 text-tinherit text-w'>{message.content}</p>
+      <div className='flex gap-2'>
+        <div className='flex size-8 flex-shrink-0 items-center justify-center rounded-full bg-primary'>
+          <Ai styles='w-5 h-5 fill-b' />
+        </div>
+
+        <Markdown
+          options={{
+            overrides: {
+              p: ({ children }) => <p className='mb-2 whitespace-pre-wrap text-tinherit text-w'>{children}</p>,
+              ul: ({ children }) => <ul className='mb-2 ml-2 flex list-disc flex-col gap-1'>{children}</ul>,
+              li: ({ children }) => <li className='whitespace-pre-wrap text-tinherit text-w'>{children}</li>,
+              span: ({ children }) => <span className='whitespace-pre-wrap text-tinherit text-w'>{children}</span>,
+              a: ({ children, ...props }) => (
+                <a href={props.href} target='_blank' className='whitespace-pre-wrap text-tinherit text-blue-400'>
+                  {children}
+                </a>
+              )
+            }
+          }}
+        >
+          {message.content}
+        </Markdown>
       </div>
     )
-  }
-
-  return (
-    <div className='flex gap-2'>
-      <div className='flex size-8 flex-shrink-0 items-center justify-center rounded-full bg-primary'>
-        <Ai styles='w-5 h-5 fill-b' />
-      </div>
-
-      <Markdown
-        options={{
-          overrides: {
-            p: ({ children }) => <p className='mb-2 whitespace-pre-wrap text-tinherit text-w'>{children}</p>,
-            ul: ({ children }) => <ul className='mb-2 ml-2 flex list-disc flex-col gap-1'>{children}</ul>,
-            li: ({ children }) => <li className='whitespace-pre-wrap text-tinherit text-w'>{children}</li>,
-            span: ({ children }) => <span className='whitespace-pre-wrap text-tinherit text-w'>{children}</span>,
-            a: ({ children, ...props }) => (
-              <a href={props.href} target='_blank' className='whitespace-pre-wrap text-tinherit text-blue-400'>
-                {children}
-              </a>
-            )
-          }
-        }}
-      >
-        {message.content}
-      </Markdown>
-    </div>
-  )
-})
+  },
+  (oldProps, newProps) => oldProps.message.content === newProps.message.content
+)
