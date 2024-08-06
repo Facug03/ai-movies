5d7e360fbab863a55ae035fdda46e3c084f66d9e
Facug03 <facundogonzales1550@gmail.com>
2024-08-05 22:55:05 -0300

    fix responsive

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -5,30 +5,30 @@ import { pages } from '@/consts/pages'
 export function Footer() {
   return (
     <footer className='mt-8 w-full max-w-[86.25rem] rounded-lg bg-neutral-800 px-6 py-3'>
-      <Link href='/' className='mb-8 block text-m-t6 font-bold text-w'>
+      <Link href='/' className='mb-8 block text-m-t7 font-bold text-w'>
         <span className='text-primary'>AI</span>MOVIES
       </Link>
 
-      <div className='flex max-w-[30%] justify-between'>
+      <div className='flex gap-8 sm:max-w-[50%] sm:justify-between lg:max-w-[30%]'>
         <div className='flex flex-col gap-3'>
-          <h5 className='text-m-t6 font-semibold uppercase text-w-50 sm:text-t6'>Discover</h5>
+          <h5 className='text-m-t7 font-semibold uppercase text-w-50 sm:text-t6'>Discover</h5>
           {pages.map((page) => (
-            <Link className='text-m-t6 text-w-75 sm:text-t6' href={page.href} key={page.name}>
+            <Link className='text-m-t7 text-w-75 sm:text-t6' href={page.href} key={page.name}>
               {page.name}
             </Link>
           ))}
         </div>
 
         <div className='flex flex-col gap-3'>
-          <h5 className='text-m-t6 font-semibold uppercase text-w-50 sm:text-t6'>Let&apos;s Connect</h5>
-          <a className='text-m-t6 text-w-75 sm:text-t6' href='mailto:facundogonzales1550@gmail.com'>
+          <h5 className='text-m-t7 font-semibold uppercase text-w-50 sm:text-t6'>Let&apos;s Connect</h5>
+          <a className='text-m-t7 text-w-75 sm:text-t6' href='mailto:facundogonzales1550@gmail.com'>
             Contact me
           </a>
-          <a className='text-m-t6 text-w-75 sm:text-t6' href='https://github.com/Facug03' target='_blank'>
+          <a className='text-m-t7 text-w-75 sm:text-t6' href='https://github.com/Facug03' target='_blank'>
             Github
           </a>
           <a
-            className='text-m-t6 text-w-75 sm:text-t6'
+            className='text-m-t7 text-w-75 sm:text-t6'
             href='https://www.linkedin.com/in/facundogonza/'
             target='_blank'
           >
@@ -39,11 +39,11 @@ export function Footer() {
 
       <hr className='my-8 h-[1px] w-full border-w-50' />
 
-      <p className='text-m-t6 text-w-75 sm:text-t7'>Made by Facu</p>
-      <p className='text-m-t6 text-w-75 sm:text-t7'>
+      <p className='mb-2 text-m-t7 text-w-75 sm:text-t7'>Made by Facu</p>
+      <p className='text-m-t7 text-w-75 sm:text-t7'>
         This product uses the TMDB API but is not endorsed or certified by{' '}
         <a
-          className='text-m-t6 underline underline-offset-4 sm:text-t7'
+          className='text-m-t7 underline underline-offset-4 sm:text-t7'
           href='https://www.themoviedb.org'
           target='_blank'
         >
