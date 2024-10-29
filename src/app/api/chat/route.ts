1c69f3baff08fe71a81c4eed9766d1bdf872beb2
Facug03 <facundogonzales1550@gmail.com>
2024-10-29 14:18:53 -0300

    feat: dont use latest version of gemini model

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
   const { messages } = await req.json()
 
   const result = await streamText({
-    model: google('models/gemini-1.5-flash-latest'),
+    model: google('models/gemini-1.5-flash'),
     messages
   })
 
