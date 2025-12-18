16a4e9f670999f563a82da7458c870bcfd5b2f58
Facug03 <facundogonzales1550@gmail.com>
2025-12-18 09:25:04 -0300

    feat: update to gemini 3 flash

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
   const { messages } = await req.json()
 
   const result = await streamText({
-    model: google('models/gemini-2.5-flash'),
+    model: google('models/gemini-3-flash-preview'),
     messages
   })
 
